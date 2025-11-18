64aec830d238d02be8cd8be64583a377afb4f960
Michal Dudko <mdudko@ethz.ch>
2025-11-18 21:28:31 +0100

    Clean up obsolete files from initial mfvar implementation
    - Removed mfvar/ R package directory (replaced by mfbvar CRAN package)
    - Removed QUICKSTART.md (described old pipeline, not current workflow)
    - Removed output/mfvar_full_pipeline/ (obsolete output format)
    
    Current implementation uses run_mfvar_package.R with mfbvar package.
    No impact on mf-var-implementation branch.

--- submission/mfvar/run_pipeline.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-Rscript run_full_pipeline.R
--- submission/mfvar/tests/testthat.R
+++
@@ -1,5 +0,0 @@
-# This file is part of the standard testthat structure
-library(testthat)
-library(mfvarR)
-
-test_check("mfvarR")
